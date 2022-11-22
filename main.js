514a16fcbc46a8449f47aa2f2d15ab46cc47a104
Angeleek <73391803+AngelP8980@users.noreply.github.com>
2022-11-22 18:50:41 +0100

    Ajout commentaires

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 "use strict"
 
+// Affiche après chargement de la page
+
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Yes easy');
   });
@@ -8,7 +10,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('DOMContentLoaded', console.log('Yes easy') );
 
-//-------------------------------------------------------------------------------------
+//
+// Affiche après clic bouton
 
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('xhr-button').addEventListener('click', onClickXHRButton)
@@ -18,8 +21,8 @@ function onClickXHRButton(){
     console.log("J'ai cliqué c'est super la vie !");
 }
 
-//-------------------------------------------------------------------------------------
-
+//
+// Affiche après clic bouton dans un endroit précis
 
 function onClickXHRButton(){
     let container = document.getElementById('xhr-response');
